5b0436e25625c7a630ffedf5ef1043b898358025
Krateng <git.noreply@krateng.ch>
2018-11-29 18:24:31 +0100

    Fixed scrobbler for tracks longer than 1 hour

--- scrobbler-vivaldi-plex/contentScript.js
+++ scrobbler-vivaldi-plex/contentScript.js
@@ -10,7 +10,13 @@ metadata = bar.querySelector("div[class*=PlayerControlsMetadata-container]")
 title = metadata.querySelector("a[class*=MetadataPosterTitle-singleLineTitle]").getAttribute("title")
 artist = metadata.querySelector("span[class*=MetadataPosterTitle-title] > a:nth-child(1)").getAttribute("title")
 duration = metadata.querySelector("[data-qa-id=mediaDuration]").innerHTML.split("/")[1]
-durationSeconds = parseInt(duration.split(":")[0]) * 60 + parseInt(duration.split(":")[1])
+if (duration.split(":").length == 2) {
+	durationSeconds = parseInt(duration.split(":")[0]) * 60 + parseInt(duration.split(":")[1])
+}
+else {
+	durationSeconds = parseInt(duration.split(":")[0]) * 60 * 60 + parseInt(duration.split(":")[1]) * 60 + parseInt(duration.split(":")[2])
+}
+
 
 control = bar.querySelector("div[class*=PlayerControls-buttonGroupCenter] > button:nth-child(2)").getAttribute("title")
 if (control == "Play") {
