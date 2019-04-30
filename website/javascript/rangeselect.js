6976014d10a925e99b6e32c3eac5254e959efd7e
Krateng <git.noreply@krateng.ch>
2019-04-30 18:47:33 +0200

    Reverted design trick to avoid weirdness, gotta think about sth better

--- website/javascript/rangeselect.js
+++ website/javascript/rangeselect.js
@@ -2,8 +2,10 @@ function showRange(identifier,unit) {
 	// Make all modules disappear
 	modules = document.getElementsByClassName("stat_module_" + identifier)
 	for (var i=0;i<modules.length;i++) {
-		modules[i].setAttribute("style","width:0px;overflow:hidden;")
+		//modules[i].setAttribute("style","width:0px;overflow:hidden;")
 		// cheesy trick to make the allocated space always whatever the biggest module needs
+		// somehow that messes up pulse on the start page tho
+		modules[i].setAttribute("style","display:none;")
 	}
 
 	// Make requested module appear
