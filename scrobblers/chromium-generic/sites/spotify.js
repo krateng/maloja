2944830df6be515f0d124d58b756117a7ef726f1
Krateng <git.noreply@krateng.ch>
2019-05-17 18:02:08 +0200

    Fixed multi-artist recognition for Spotify

--- scrobblers/chromium-generic/sites/spotify.js
+++ scrobblers/chromium-generic/sites/spotify.js
@@ -4,7 +4,8 @@ maloja_scrobbler_selector_playbar = "//div[@class='now-playing-bar']"
 maloja_scrobbler_selector_metadata = ".//div[@class='now-playing-bar__left']"
 
 maloja_scrobbler_selector_title = ".//div[contains(@class,'track-info__name')]//a/text()"
-maloja_scrobbler_selector_artist = ".//div[contains(@class,'track-info__artists')]//a/text()"
+maloja_scrobbler_selector_artists = ".//div[contains(@class,'track-info__artists')]//a"
+maloja_scrobbler_selector_artist = "./text()"
 maloja_scrobbler_selector_duration = ".//div[@class='playback-bar__progress-time'][2]/text()"
 
 
