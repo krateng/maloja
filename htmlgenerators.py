65afef848922145138b6cf7048185806f49bc903
Krateng <git.noreply@krateng.ch>
2019-06-10 17:42:08 +0200

    Showing artists and title in one column

--- htmlgenerators.py
+++ htmlgenerators.py
@@ -14,8 +14,9 @@ def entity_column(element,counting=[],image=None):
 
 	if "artists" in element:
 		# track
-		html += "<td class='artists'>" + html_links(element["artists"]) + "</td>"
-		html += "<td class='title'>" + html_link(element) + "</td>"
+	#	html += "<td class='artists'>" + html_links(element["artists"]) + "</td>"
+	#	html += "<td class='title'>" + html_link(element) + "</td>"
+		html += "<td class='title'>" + html_links(element["artists"]) + " – " + html_link(element) + "</td>"
 	else:
 		# artist
 		html += "<td class='artist'>" + html_link(element)
