728c4fed54b6bc8a3a335e9b53cbe606d9815354
Krateng <git.noreply@krateng.ch>
2019-06-26 17:17:15 +0200

    Fixed description text for performance / pulse

--- malojatime.py
+++ malojatime.py
@@ -546,9 +546,8 @@ def time_stamps(since=None,to=None,within=None,range=None):
 
 def delimit_desc(step="month",stepn=1,trail=1):
 	txt = ""
-	if stepn is not 1: txt += _num(stepn) + "-"
+	if stepn is not 1: txt += str(stepn) + "-"
 	txt += {"year":"Yearly","month":"Monthly","week":"Weekly","day":"Daily"}[step.lower()]
-	#if trail is not 1: txt += " " + _num(trail) + "-Trailing"
 	if trail is math.inf: txt += " Cumulative"
 	elif trail is not 1: txt += " Trailing" #we don't need all the info in the title
 
