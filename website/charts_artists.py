864e7bc46d0998ed936c594cfccfa6b6d5ff70ba
Zack Boehm <git@zackboehm.com>
2019-09-04 01:58:28 -0400

    Optionally display tiles in artist + track chart pages
    This allows for viewing top tiles for any specific timerange

--- website/charts_artists.py
+++ website/charts_artists.py
@@ -4,8 +4,9 @@ import urllib
 def instructions(keys):
 	from utilities import getArtistImage
 	from urihandler import compose_querystring, uri_to_internal
-	from htmlmodules import module_artistcharts, module_filterselection
+	from htmlmodules import module_artistcharts, module_filterselection, module_artistcharts_tiles
 	from malojatime import range_desc
+	from doreah.settings import get_settings
 
 
 	_, timekeys, _, amountkeys = uri_to_internal(keys)
@@ -14,6 +15,9 @@ def instructions(keys):
 
 	html_filterselector = module_filterselection(keys)
 
+	topartists = ""
+	if get_settings("CHARTS_DISPLAY_TILES"):
+		topartists = module_artistcharts_tiles(timerange=timekeys["timerange"])
 
 
 	html_charts, rep = module_artistcharts(**amountkeys,**timekeys)
@@ -23,10 +27,16 @@ def instructions(keys):
 	else:
 		imgurl = ""
 
+	imgdiv = '<div style="background-image:url('+imgurl+')"></div>'
+	if get_settings("CHARTS_DISPLAY_TILES"):
+		imgdiv = ""
+
 	pushresources = [{"file":imgurl,"type":"image"}] if imgurl.startswith("/") else []
 
 
-	replace = {"KEY_TOPARTIST_IMAGEURL":imgurl,
+	replace = {
+	"KEY_TOPARTIST_IMAGEDIV":imgdiv,
+	"KEY_ARTISTCHART":topartists,
 	"KEY_ARTISTLIST":html_charts,
 	"KEY_RANGE":limitstring,
 	"KEY_FILTERSELECTOR":html_filterselector}
--- website/charts_tracks.py
+++ website/charts_tracks.py
@@ -5,8 +5,9 @@ def instructions(keys):
 	from utilities import getArtistImage, getTrackImage
 	from htmlgenerators import artistLink
 	from urihandler import compose_querystring, uri_to_internal
-	from htmlmodules import module_trackcharts, module_filterselection
+	from htmlmodules import module_trackcharts, module_filterselection, module_trackcharts_tiles
 	from malojatime import range_desc
+	from doreah.settings import get_settings
 
 	filterkeys, timekeys, _, amountkeys = uri_to_internal(keys)
 
@@ -22,6 +23,10 @@ def instructions(keys):
 
 	html_charts, rep = module_trackcharts(**amountkeys,**timekeys,**filterkeys)
 
+	toptracks = ""
+	if get_settings("CHARTS_DISPLAY_TILES"):
+		toptracks = module_trackcharts_tiles(timerange=timekeys["timerange"])
+
 
 	if filterkeys.get("artist") is not None:
 		imgurl = getArtistImage(filterkeys.get("artist"))
@@ -31,6 +36,10 @@ def instructions(keys):
 	else:
 		imgurl = ""
 
+	imgdiv = '<div style="background-image:url('+imgurl+')"></div>'
+	if get_settings("CHARTS_DISPLAY_TILES"):
+		imgdiv = ""
+
 	limitstring += " " + timekeys["timerange"].desc(prefix=True)
 
 	pushresources = [{"file":imgurl,"type":"image"}] if imgurl.startswith("/") else []
@@ -38,7 +47,8 @@ def instructions(keys):
 
 
 	replace = {
-		"KEY_TOPARTIST_IMAGEURL":imgurl,
+		"KEY_TOPARTIST_IMAGEDIV":imgdiv,
+		"KEY_TRACKCHART":toptracks,
 		"KEY_TRACKLIST":html_charts,
 		"KEY_LIMITS":limitstring,
 		"KEY_FILTERSELECTOR":html_filterselector,
