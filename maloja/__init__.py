6369cbbeb8721a7fe3ad0a3973ca9aa5d916a35f
Krateng <git.noreply@krateng.ch>
2019-12-13 21:04:12 +0100

    Added first partial pyhp modules

--- maloja/__init__.py
+++ maloja/__init__.py
@@ -7,14 +7,14 @@ author = {
 	"email":"maloja@krateng.dev",
 	"github": "krateng"
 }
-version = 2,1,4
+version = 2,2,0
 versionstr = ".".join(str(n) for n in version)
 
 
 requires = [
 	"bottle>=0.12.16",
 	"waitress>=1.3",
-	"doreah>=1.2.10",
+	"doreah>=1.4",
 	"nimrodel>=0.6.3",
 	"setproctitle>=1.1.10",
 	"wand>=0.5.4",
