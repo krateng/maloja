0ceb70b27e9c7e0be25059a7754244a1b65e0624
Krateng <git.noreply@krateng.ch>
2020-03-23 17:24:58 +0100

    Updated Spotify scrobbling

--- scrobblers/chromium/sites/spotify.js
+++ scrobblers/chromium/sites/spotify.js
@@ -3,8 +3,8 @@ maloja_scrobbler_selector_playbar = "//div[@class='now-playing-bar']"
 
 maloja_scrobbler_selector_metadata = ".//div[@class='now-playing-bar__left']"
 
-maloja_scrobbler_selector_title = ".//div[contains(@class,'track-info__name')]//a/text()"
-maloja_scrobbler_selector_artists = ".//div[contains(@class,'track-info__artists')]//a"
+maloja_scrobbler_selector_title = ".//a[@data-testid='nowplaying-track-link']/text()"
+maloja_scrobbler_selector_artists = ".//a[contains(@href,'/artist/')]"
 maloja_scrobbler_selector_artist = "./text()"
 maloja_scrobbler_selector_duration = ".//div[@class='playback-bar__progress-time'][2]/text()"
 
