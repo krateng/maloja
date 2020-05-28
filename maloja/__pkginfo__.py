d989134e65c20ab33b0ea8e4a132655074057757
Krateng <git.noreply@krateng.ch>
2020-05-28 19:45:30 +0200

    Fixed some CSS

--- maloja/__pkginfo__.py
+++ maloja/__pkginfo__.py
@@ -5,7 +5,7 @@ author = {
 	"email":"maloja@krateng.dev",
 	"github": "krateng"
 }
-version = 2,4,1
+version = 2,4,2
 versionstr = ".".join(str(n) for n in version)
 links = {
 	"pypi":"malojaserver",
