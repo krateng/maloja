1dbc0b7fca05830d654076c74a91b6b74f470d5b
Krateng <git.noreply@krateng.ch>
2020-09-02 14:28:04 +0200

    Merged ramining references to two uri modules

--- maloja/__pkginfo__.py
+++ maloja/__pkginfo__.py
@@ -5,7 +5,7 @@ author = {
 	"email":"maloja@krateng.dev",
 	"github": "krateng"
 }
-version = 2,8,4
+version = 2,8,5
 versionstr = ".".join(str(n) for n in version)
 links = {
 	"pypi":"malojaserver",
