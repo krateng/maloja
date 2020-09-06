a462faf2bfa3251ebd327293f7f78e546bc3c9a4
Krateng <git.noreply@krateng.ch>
2020-09-06 18:19:48 +0200

    Changed redirect to more appropriate HTTP code

--- maloja/server.py
+++ maloja/server.py
@@ -63,12 +63,12 @@ init_apis(webserver)
 @webserver.get("/api/s/<pth:path>")
 @webserver.post("/api/s/<pth:path>")
 def deprecated_api_s(pth):
-	redirect("/apis/" + pth + "?" + request.query_string,307)
+	redirect("/apis/" + pth + "?" + request.query_string,308)
 
 @webserver.get("/api/<pth:path>")
 @webserver.post("/api/<pth:path>")
 def deprecated_api(pth):
-	redirect("/apis/mlj_1/" + pth + "?" + request.query_string,307)
+	redirect("/apis/mlj_1/" + pth + "?" + request.query_string,308)
 
 
 
