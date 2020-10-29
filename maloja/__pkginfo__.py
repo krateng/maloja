f555ee9d9fc485c6a241f4a8fa88bd68527ed2e2
Krateng <git.noreply@krateng.ch>
2020-10-29 17:26:23 +0100

    Version bump

--- maloja/__pkginfo__.py
+++ maloja/__pkginfo__.py
@@ -5,7 +5,7 @@ author = {
 	"email":"maloja@krateng.dev",
 	"github": "krateng"
 }
-version = 2,10,0
+version = 2,10,1
 versionstr = ".".join(str(n) for n in version)
 links = {
 	"pypi":"malojaserver",
