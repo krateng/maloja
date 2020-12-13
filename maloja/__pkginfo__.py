218313f80c160f90b28d99236a062ef62db7260d
Krateng <git.noreply@krateng.ch>
2020-12-13 03:24:40 +0100

    Fixed requirement

--- maloja/__pkginfo__.py
+++ maloja/__pkginfo__.py
@@ -5,7 +5,7 @@ author = {
 	"email":"maloja@krateng.dev",
 	"github": "krateng"
 }
-version = 2,10,7
+version = 2,11,0
 versionstr = ".".join(str(n) for n in version)
 links = {
 	"pypi":"malojaserver",
@@ -15,7 +15,7 @@ links = {
 requires = [
 	"bottle>=0.12.16",
 	"waitress>=1.3",
-	"doreah>=1.6.13",
+	"doreah>=1.6.12",
 	"nimrodel>=0.6.4",
 	"setproctitle>=1.1.10",
 	"wand>=0.5.4",
