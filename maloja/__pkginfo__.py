cdd762a07a5a101fe31152580453e79f293bc53d
krateng <git.noreply@krateng.ch>
2021-12-25 22:02:28 +0100

    Adjusted pkginfo

--- maloja/__pkginfo__.py
+++ maloja/__pkginfo__.py
@@ -1,11 +1,19 @@
 # This file has now been slighly repurposed and will simply give other parts of
 # the package access to some global meta-information about itself
 
-import pkg_resources
-self = pkg_resources.get_distribution('maloja')
+try:
+	# package distributrion
+	import pkg_resources
+	self = pkg_resources.get_distribution('maloja')
+	metadata = {'version':self.version}
+except:
+	# from source
+	import toml
+	with open("./pyproject.toml") as filed:
+		metadata = toml.load(filed)['project']
 
-versionstr = self.version
-version = self.version.split('.')
+versionstr = metadata['version']
+version = versionstr.split('.')
 urls = {
 	"repo":"https://github.com/krateng/maloja"
 }
