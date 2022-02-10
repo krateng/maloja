d3797d89fa2bfee1bb505da3df6a2ce07a46f8ef
krateng <git.noreply@krateng.ch>
2022-02-10 08:32:44 +0100

    Fixed Plex scrobbling again

--- auxiliary/chromium_scrobbler/maloja-scrobbler/sites/plex.js
+++ auxiliary/chromium_scrobbler/maloja-scrobbler/sites/plex.js
@@ -5,7 +5,7 @@ maloja_scrobbler_selector_metadata = ".//div[contains(@class,'PlayerControlsMeta
 
 maloja_scrobbler_selector_title = ".//a[@data-testid='metadataTitleLink']/@title"
 maloja_scrobbler_selector_artist = ".//span[contains(@class,'MetadataPosterTitle-title')]/a[1]/@title"
-maloja_scrobbler_selector_duration = ".//button[@data-qa-id='mediaDuration']/text()[3]"
+maloja_scrobbler_selector_duration = ".//button[@data-testid='mediaDuration']/text()[3]"
 
 
 maloja_scrobbler_selector_control = ".//div[contains(@class,'PlayerControls-buttonGroupCenter')]/button[2]/@title"
