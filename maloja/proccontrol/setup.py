38e2a184af59a24805be6c0ab34359bc11757362
krateng <git.noreply@krateng.ch>
2022-02-13 05:56:36 +0100

    Added password confirmation prompt

--- maloja/proccontrol/setup.py
+++ maloja/proccontrol/setup.py
@@ -63,12 +63,16 @@ def setup():
 		print("Password has been set.")
 	elif auth.defaultuser.checkpw("admin"):
 		# if the actual pw is admin, it means we've never set this up properly (eg first start after update)
-		newpw = prompt("Please set a password for web backend access. Leave this empty to generate a random password.",skip=SKIP,secret=True)
-		if newpw is None:
-			newpw = randomstring(32)
-			print("Generated password:",col["yellow"](newpw))
-
-		auth.defaultuser.setpw(newpw)
+		newpw, newpw_repeat = 1, 2
+		while newpw != newpw_repeat:
+			newpw = prompt("Please set a password for web backend access. Leave this empty to generate a random password.",skip=SKIP,secret=True)
+			if newpw is None:
+				newpw = randomstring(32)
+				print("Generated password:",col["yellow"](newpw))
+			else:
+				newpw_repeat = prompt("Please type again to confirm.",skip=SKIP,secret=True)
+				if newpw != newpw_repeat: print("Passwords do not match!")
+				else: auth.defaultuser.setpw(newpw)
 
 	if malojaconfig["SEND_STATS"] is None:
 		answer = ask("I would like to know how many people use Maloja. Would it be okay to send a daily ping to my server (this contains no data that isn't accessible via your web interface already)?",default=True,skip=SKIP)
