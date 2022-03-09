e3dc401ccf1f7b46ea814cf58070b8ac87ea22c0
krateng <git.noreply@krateng.ch>
2022-03-09 21:53:42 +0100

    Automated all distribution files

--- dev/dockertest.sh
+++ dev/dockertest.sh
@@ -1,2 +1,2 @@
-docker build -t maloja-dev . -f Dockerfile-dev
-docker run -p 42010:42010 -v $PWD/testdata:/mlj -e MALOJA_DATA_DIRECTORY=/mlj maloja-dev
+docker build -t maloja . -f Dockerfile
+docker run --rm -p 42010:42010 -v $PWD/testdata:/mlj -e MALOJA_DATA_DIRECTORY=/mlj maloja
--- install_alpine.sh
+++ install_alpine.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 apk add \
-	python3 \
-	python3-dev \
 	gcc \
+	python3-dev \
 	libxml2-dev \
 	libxslt-dev \
 	libffi-dev \
 	libc-dev \
 	py3-pip \
 	linux-headers \
+	python3 \
 	tzdata \
 	vips
 pip3 install wheel
