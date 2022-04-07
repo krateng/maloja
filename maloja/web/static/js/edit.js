8d111b6de711646d49152c15ac0549b629244f36
krateng <git.noreply@krateng.ch>
2022-04-07 20:23:03 +0200

    Improved design of scrobble delete functionality

---
+++ maloja/web/static/js/edit.js
@@ -0,0 +1,10 @@
+// JS for all web interface editing / deletion of scrobble data
+
+function toggleDeleteConfirm(element) {
+	element.parentElement.parentElement.classList.toggle('active');
+}
+
+function deleteScrobble(id,element) {
+	element.parentElement.parentElement.parentElement.classList.add('removed');
+
+}
