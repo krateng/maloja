c33fcf1dc1e5c390856a50d902aa930e4ce2ff1e
krateng <git.noreply@krateng.ch>
2022-04-15 18:16:54 +0200

    Added edit function to web interface

--- maloja/web/static/js/edit.js
+++ maloja/web/static/js/edit.js
@@ -10,3 +10,42 @@ function deleteScrobble(id,element) {
 	neo.xhttpreq("/apis/mlj_1/delete_scrobble",data={'timestamp':id},method="POST",callback=(()=>null),json=true);
 
 }
+
+
+function selectAll(e) {
+	// https://stackoverflow.com/a/6150060/6651341
+	var range = document.createRange();
+    range.selectNodeContents(e);
+	var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+}
+
+function editEntity() {
+	var namefield = document.getElementById('main_entity_name');
+	namefield.contentEditable = "plaintext-only";
+
+	// dont allow new lines, done on enter
+	namefield.addEventListener('keypress',function(e){
+		if (e.which === 13) {
+			e.preventDefault();
+			doneEditing();
+		}
+
+	})
+	// emergency, not pretty because it will move cursor
+	namefield.addEventListener('input',function(e){
+		if (namefield.innerHTML.includes("\n")) {
+			namefield.innerHTML = namefield.innerHTML.replace("\n","");
+		}
+
+	})
+
+	namefield.focus();
+	selectAll(namefield);
+}
+
+function doneEditing() {
+	var namefield = document.getElementById('main_entity_name');
+	namefield.contentEditable = "false";
+}
