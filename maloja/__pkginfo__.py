5495d6e38d95c0c2128e1de9a9553b55b6be945b
krateng <git.noreply@krateng.ch>
2023-11-02 00:36:57 +0100

    Version bump

--- maloja/__pkginfo__.py
+++ maloja/__pkginfo__.py
@@ -4,7 +4,7 @@
 # you know what f*ck it
 # this is hardcoded for now because of that damn project / package name discrepancy
 # i'll fix it one day
-VERSION = "3.2.0"
+VERSION = "3.2.1"
 HOMEPAGE = "https://github.com/krateng/maloja"
 
 
