273713cdc4ed9bec5f95f13a81a283d53e703c58
krateng <git.noreply@krateng.ch>
2025-01-19 03:00:00 +0100

    Simpler dev testing with compose

--- dev/run_docker.sh
+++
@@ -1,2 +0,0 @@
-docker build -t maloja . -f Containerfile
-docker run --rm -p 42010:42010 -v $PWD/testdata:/mlj -e MALOJA_DATA_DIRECTORY=/mlj maloja
--- dev/run_podman.sh
+++
@@ -1,2 +0,0 @@
-podman build -t maloja . -f Containerfile
-podman run --rm -p 42010:42010 -v $PWD/testdata:/mlj -e MALOJA_DATA_DIRECTORY=/mlj maloja
